6db643d408f357f874c40cefd430b132d7c6a425
Raphael Michl <raphael.michl@protonmail.com>
2020-02-09 15:55:06 +0100

    Added formControl functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { SimpleSelectModule } from 'projects/simple-select/src/lib/simple-select.module';
+
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SimpleSelectModule } from 'projects/simple-select/src/lib/simple-select.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { SimpleSelectModule } from 'projects/simple-select/src/lib/simple-select
   ],
   imports: [
     BrowserModule,
-    SimpleSelectModule
+    ReactiveFormsModule,
+    SimpleSelectModule // SimpleSelectModule already exports FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
